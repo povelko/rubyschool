cea38d87901bd8e2f76207e75cf83b5e195da9f5
povelko <povelkoea@gmail.com>
2021-02-17 10:02:21 +0600

    update app

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 get '/about' do
-	@error = 'Ошибка!!!'
 	erb :about
 end
 
