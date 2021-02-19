90f713607aff599c6b304a3e1fc2a71771822da2
povelko <povelkoea@gmail.com>
2021-02-19 14:37:09 +0600

    add sql zub,add record

--- sqlite.rb
+++ sqlite.rb
@@ -1,2 +1,10 @@
 require 'sqlite3'
-db = SQLite3::Database.new 'zuby'
+db = SQLite3::Database.new 'zub'
+
+db.execute "insert into contacts (email, message) values('povelkoea@gmail.com','Programist iz Znamenskoe')"
+db.execute "select * from contacts" do |data|
+	puts data
+end
+
+db.close
+#insert into cars (name, price) values ('rav4', 10000)
